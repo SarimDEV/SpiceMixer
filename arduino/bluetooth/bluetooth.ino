bc5db794d7571aa78c5aa72f2cc6453269bf0a17
Sahifa Shahid <sahifa@v1040-wn-rt-a-74-136.campus-dynamic.uwaterloo.ca>
2023-02-24 18:04:54 -0500

    completed app and ble integration

--- arduino/bluetooth/bluetooth.ino
+++ arduino/bluetooth/bluetooth.ino
@@ -1,10 +1,15 @@
 #include <SoftwareSerial.h>
-#define MAX_SIZE 12
 
 SoftwareSerial HM10(2, 3); // RX = 0, TX = 1
 char appData;
 String inData = "";
 
+String rawData = "";
+String dataType = "";
+String data = "";
+
+int configArr[12];
+int numContainers = 0;
 
 String getValue(String data, char separator, int index) {
     int found = 0;
@@ -22,7 +27,6 @@ String getValue(String data, char separator, int index) {
 }
 
 int recipeParser(String data, int arr[][2]) {
-  
   String currentNum = "";
   int index = 0;
   int subIndex = 0;
@@ -49,24 +53,35 @@ int recipeParser(String data, int arr[][2]) {
   return index;
 }
 
+int configurationParser(String data, int arr[]) {
+  String currentNum = "";
+  int index = 0;
+
+  for (int i = 0; i < data.length(); i++) {
+    if (isDigit(data[i]) == true) {
+      currentNum += data[i];
+    }
+    else {
+      arr[index] = currentNum.toInt();
+      currentNum = "";
+      index += 1;
+    }
+  }
+
+  if (currentNum != "") {
+    arr[index] = currentNum.toInt();
+    index += 1;
+  }
+
+  return index;
+}
+
 void setup() {
   Serial.begin(9600);
   Serial.println("HM10 serial started at 9600");
   HM10.begin(9600); // set HM10 serial at 9600 baud rate
   pinMode(13, OUTPUT); // onboard LED
   digitalWrite(13, LOW); // switch OFF LED
-  String dummy = "new_recipe:[[1,2],[2,4]]";
-  String dataType = getValue(dummy, ':', 0);
-  String data = getValue(dummy, ':', 1);
-
-  int arr[12][2];
-  int amount = recipeParser(data, arr);
-  Serial.println(amount);
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++) {
-      Serial.println(arr[i][j]);
-    }
-  }
 }
 
 
@@ -76,18 +91,57 @@ void loop() {
     appData = HM10.read();
     inData = String(appData);  // save the data in string format
     Serial.write(appData);
+
+    if (appData == '!') {
+      rawData = "";
+    }
+
+    if (appData != '!' && appData != '?') {
+      rawData += inData;
+    }
+
+    if (appData == '?') {
+      Serial.println();
+      dataType = getValue(rawData, ':', 0);
+      data = getValue(rawData, ':', 1);
+    }
   }
 
-  if (Serial.available()) {           // Read user input if available.
-    HM10.write(Serial.read());
+  if (dataType == "C") {
+    Serial.println("configuring machine...");
+    digitalWrite(13, HIGH); // switch ON LED
+
+    numContainers = configurationParser(data, configArr);
+    Serial.println(numContainers);
+
+    dataType = "";
+    data = "";
+
+    // for (int i = 0; i < numContainers; i++) {
+    //   Serial.println(configArr[i]);
+    // }
+
+    Serial.println("successfully configured machine!");
+
   }
 
-  if ( inData == "F") {
-    digitalWrite(13, LOW); // switch OFF LED
-    delay(500);
-  }
+  if (dataType == "N") {
+    Serial.println("parsing recipe data...");
+    digitalWrite(13, HIGH); // switch ON LED
 
-  if ( inData == "N") {
-    digitalWrite(13, HIGH); // switch OFF LED
+    int arr[12][2];
+    int amount = recipeParser(data, arr);
+    Serial.println(amount);
+
+    dataType = "";
+    data = "";
+
+    // for (int i = 0; i < amount; i++) {
+    //   for (int j = 0; j < 2; j++) {
+    //     Serial.println(arr[i][j]);
+    //   }
+    // }
+
+    Serial.println("successfully saved recipe!");
   }
 }
--- arduino/testing_code/bluetooth_led_test/bluetooth_led_test.ino
+++ arduino/testing_code/bluetooth_led_test/bluetooth_led_test.ino
@@ -8,17 +8,18 @@ void setup() {
   Serial.begin(9600);
   Serial.println("HM10 serial started at 9600");
   HM10.begin(9600); // set HM10 serial at 9600 baud rate
-  pinMode(13, OUTPUT); // onboard LED
-  digitalWrite(13, LOW); // switch OFF LED
+  pinMode(12, OUTPUT); // onboard LED
+  digitalWrite(12, LOW); // switch OFF LED
 }
 
 
 void loop() {
   HM10.listen();  // listen the HM10 port
+  Serial.println("Listening")
   while (HM10.available() > 0) {   // if HM10 sends something then read
     appData = HM10.read();
     inData = String(appData);  // save the data in string format
-    Serial.write(appData);
+    Serial.write("APP DATA: ", appData);
   }
 
   if (Serial.available()) {           // Read user input if available.
@@ -27,15 +28,15 @@ void loop() {
 
   if ( inData == "F") {
     Serial.println("LED OFF");
-    digitalWrite(13, LOW); // switch OFF LED
+    digitalWrite(12, LOW); // switch OFF LED
     delay(500);
   }
 
   if ( inData == "N") {
     Serial.println("LED ON");
-    digitalWrite(13, HIGH); // switch OFF LED
+    digitalWrite(12, HIGH); // switch OFF LED
     delay(500);
-    digitalWrite(13, LOW); // switch OFF LED
+    digitalWrite(12, LOW); // switch OFF LED
     delay(500);
   }
 }
