0b7743e4db058910e83f8970b3d66e4624a037c4
Sahifa Shahid <sahifa@v1040-wn-rt-a-18-8.campus-dynamic.uwaterloo.ca>
2023-03-10 00:40:54 -0500

    modified motor logic

--- arduino/motors/motors.ino
+++ arduino/motors/motors.ino
@@ -1,105 +1,83 @@
-#include <Stepper.h>
-#include <stdio.h>
-#include <ezButton.h>
+// Include the AccelStepper Library
+#include <AccelStepper.h>
 
-int spicePositions[12] = {0};
+// Define pin connections
+const int dirPin = 2;
+const int stepPin = 3;
+const int linPin = 4;
 
-// NOTE: I've hardcoded the max # of recipes we're storing as 10,
-// cause fuck dynamically allocating a 2D array in C
-int recipes[10][13] = {0};
-// NOTE: the 0th element of each recipe slot will be set to -1 when it is used
-// so new recipes will only enter empty slots where recipes[i][0] != -1
+// Define motor interface type
+#define motorInterfaceType 1
 
-// change this to fit the number of steps per revolution
-// for your motor
-const int stepsPerRevolution = 200;
-const float stepAngle = 200.0 / 360.0;
-const float stepsPerSpice = 30.0 / stepAngle;
+// Creates an instance
+AccelStepper myStepper(motorInterfaceType, stepPin, dirPin);
 
-// TODO: change pins according to reality (currently placeholder numbers)
-Stepper pickerStepper(stepsPerRevolution, 1, 2, 3, 4);
-Stepper dispenserStepper(stepsPerRevolution, 5, 6, 7, 8);
-ezButton limitSwitch(9);
+///DUMMY DATA
+int containerConfig[3] = {1,4,7};
+int recipeInfo[4][2] = { 
+  {1,0},
+  {7,4},
+  {1,0},
+  {4,0}
+};
+const int steps = -133.0*2;
+int spiceLocArray[3]={0};
 
-// spiceId is the spice identifier (integer constants associated with a spice name)
-// (e.g. int PAPRIKA = 5)
-// containerPosition is the position the spice is located (0-11)
-void store(int containerPosition, int spiceId) {
-  spicePositions[containerPosition] = spiceId;
-}
+int prev = 0;
 
-int * read_spice_locations() {
-  return spicePositions;
-}
-
-// Make index = spiceID, and value at index be # numTeaspoons
-void save_recipe(int teaspoons[12]) {
-  int i = 0;
-  
-  while (i < 12) {
-    // recipe slot is empty, can store recipe here
-    if (recipes[i][0]) != -1) {
-      memcpy(recipes[i], teaspoons, sizeof(teaspoons));
-      recipes[0] = -1;
-      break;
-    }
-
-    i++;
-  }
-}
-
-void move_to_container(int container) {
-  // go back to home
-  move_home();
-
-  // go to target spice
-  int i = 0;
-  while (i < container) {
-    pickerStepper.step(stepsPerSpice)
-    delay(1000);
-    
-    i++;
-  }
-}
-
-void move_home() {
-  bool detected = false;
-  int iterations = 0;
-  int maxIterations = 250;
-  
-  while(!detected && iterations < maxIterations){
-    pickerStepper.step(1);
-    delay(50);
-
-    iterations++;
-    detected = limitSwitch.isPressed();
-  }
-}
-
-void dispense(int amt) {
-  // fully spin dispenser servo 360 degrees three times
-  dispenserStepper.step(200);
-  delay(500);
-  dispenserStepper.step(200);
-  delay(500);
-  dispenserStepper.step(200);
-  delay(500);
-}
 
 void setup() {
-  // set the speed at 60 rpm:
-  pickerStepper.setSpeed(60);
-  dispenserStepper.setSpeed(60);
-
-  // set debounce time to 50 milliseconds
-  limitSwitch.setDebounceTime(25);
-  
-  // initialize the serial port:
+	// set the maximum speed, acceleration factor,
+	// initial speed and the target position
+	myStepper.setMaxSpeed(500);
+	myStepper.setAcceleration(800);
+	myStepper.setSpeed(500);
+ 
   Serial.begin(9600);
+  dispense(recipeInfo);
+  myStepper.moveTo(0);
+  pinMode(linPin, OUTPUT);
 }
 
 void loop() {
-  limitSwitch.loop();
+	// Change direction once the motor reaches target position
+  rotate(spiceLocArray);
+  // Move the motor one step*/
+  myStepper.run();
+}
 
-  // TODO: figure out what to put here when we maybe make a state machine???
+void dispense(int data[][2]){
+//map id to correct position
+  for (int i = 0; i < 3; i++) {//hardcoded length
+    int spiceID = data[i][0];
+    // Serial.print(spiceID);
+    int spiceLoc = 0;
+    for(int j = 0; j < 3; j++) { // find pos of spice
+      if (spiceID == containerConfig[j]) {//assume that spice is in container config, or else pos = 0
+        spiceLoc = j;
+      }
+    }
+   spiceLocArray[i]= spiceLoc;
+  }
+}
+
+ 
+void rotate(int positions[]){
+  for (int i =0; i<3; i++){
+    // Serial.print(positions[i]);
+    if (myStepper.distanceToGo()==0 && myStepper.currentPosition() == prev) {
+      //Serial.print(prev);
+      myStepper.moveTo(positions[i]*steps);
+
+      for (int j=0; j<recipeInfo[i][1]*4;j++) {
+        Serial.print(recipeInfo[i][1]);
+        digitalWrite(linPin, HIGH);
+        delay(1000);
+        digitalWrite(linPin, LOW);
+        delay(1000);
+      }
+      // Serial.print(positions[i]);
+    }
+    prev = positions[i]*steps;
+  }
 }
